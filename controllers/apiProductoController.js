710d8e0764e5ea34075eaeef3c860df0477712cf
ezzebainer <ezzebainer@gmail.com>
2026-06-27 17:32:59 -0300

    Agregando filtro por categoria

--- controllers/apiProductoController.js
+++ controllers/apiProductoController.js
@@ -1,12 +1,24 @@
 import prisma from "../src/lib/prisma.js";
 
 export const obtenerProductos = async (req, res) => {
+
   try {
 
+    const {
+      categoria
+    } = req.query;
+    console.log(req.query);
+
+    const where = {
+      activo: true
+    };
+
+    if (categoria) {
+      where.categoria = categoria;
+    }
+
     const productos = await prisma.producto.findMany({
-      where: {
-        activo: true
-      },
+      where,
       orderBy: {
         createdAt: "desc"
       }
@@ -28,4 +40,5 @@ export const obtenerProductos = async (req, res) => {
     });
 
   }
+
 };
