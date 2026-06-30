dc10c68396824d1fcbf3c7e1e940a9b0a38bd953
luchi-cicchelli <luciafcicchelli@gmail.com>
2026-06-30 12:57:35 -0300

    mejorando paginacion

--- routes/apiProductos.js
+++ routes/apiProductos.js
@@ -3,6 +3,11 @@ import {obtenerProductos, obtenerProductoPorId} from "../controllers/apiProducto
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+  res.set("Cache-Control", "no-store");
+  next();
+});
+
 router.get("/", obtenerProductos);
 router.get("/:id", obtenerProductoPorId);
 
